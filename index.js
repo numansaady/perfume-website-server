094866152e9f549acd2f77215632ae42bcfd2d40
numansaady <numansaady@gmail.com>
2022-05-04 08:29:51 +0600

    login api created with jwt token

--- index.js
+++ index.js
@@ -4,7 +4,23 @@ const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
+const jwt = require('jsonwebtoken');
 
+function verifyToken(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' });
+        }
+        console.log('decoded', decoded);
+        req.decoded = decoded;
+        next();
+    })
+}
 
 // Use middleware
 app.use(cors());
@@ -23,6 +39,15 @@ async function run(){
     try{
         await client.connect();
         const perfumeCollection = client.db("perfumeCenter").collection("perfume");
+
+         // Authenticatin with JWT
+         app.post('/login', async (req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1800'
+            });
+            res.send({ accessToken });
+        })
 
         // Perfume Collection API
 
